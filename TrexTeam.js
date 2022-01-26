37cfb0409dc68cdb0862230291d79da14adcc963
Dark-Maxv1 <98449027+Dark-Maxv1@users.noreply.github.com>
2022-01-26 15:27:16 +0530

    ⎝🛡️ ALPHA 🛡️⎠ v4.0.0

---
+++ TrexTeam.js
@@ -0,0 +1,5 @@
+module.exports = {
+    ALPHA_OWNER: '94784506970', // Trex Team - Alpha Owner
+    TREX_TEAM: '94771039631,94784506970,94716184907,94711421243,94771251788', // Team - Trex TEAM
+    TREX_OWNER: '94771039631' // Hirush Rukshan - Trex Owner --> For The Friendship And co Owner in Alpha 
+};
